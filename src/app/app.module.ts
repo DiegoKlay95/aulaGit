b8a9b9067c2598f67958ab5f23c6c9e059724b4a
DiegoKlay95 <diegoklay@gmail.com>
2019-05-15 21:58:05 -0300

    versao 3 atualizada

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { RodapeComponent } from './rodape/rodape.component';
+import { SobreComponent } from './sobre/sobre.component';
+import { Erro404Component } from './erro404/erro404.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, HomeComponent, RodapeComponent, SobreComponent, Erro404Component
   ],
   imports: [
     BrowserModule,
---
+++ src/app/erro404/erro404.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+selector:'erro.component'
+,templateUrl: 'erro404.component.html'
+,styleUrls: ['erro404.Component.css']
+})
+export class Erro404Component{
+
+}
